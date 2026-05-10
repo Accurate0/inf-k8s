b6f6885791d8536395ad7975e4ba40b4401fe577
Anurag Singh <contact@anurag.sh>
2026-05-10 16:15:25 +0800

    update renovate config

--- components/renovate/config.js
+++ components/renovate/config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  platform: "forgejo",
+  platform: "gitea",
   endpoint: "https://git.anurag.sh/api/v1",
   autodiscover: true,
   autodiscoverFilter: "/topic/renovate/",
   dependencyDashboard: true,
+  gitAuthor: "Renovate <noreply@git.anurag.sh>"
   extends: ["config:recommended"],
 };
