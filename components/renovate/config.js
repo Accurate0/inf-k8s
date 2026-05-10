a65384a5d964d9bb8d5a0544c15aae40d52c5894
Anurag Singh <contact@anurag.sh>
2026-05-10 16:37:17 +0800

    add topics discovery

--- components/renovate/config.js
+++ components/renovate/config.js
@@ -2,7 +2,7 @@ module.exports = {
   platform: "forgejo",
   endpoint: "https://git.anurag.sh/api/v1",
   autodiscover: true,
-  autodiscoverFilter: "/topic/renovate/",
+  autodiscoverTopics: ["managed-by-renovate"],
   dependencyDashboard: true,
   gitAuthor: "Renovate <noreply@git.anurag.sh>",
   extends: ["config:recommended"],
