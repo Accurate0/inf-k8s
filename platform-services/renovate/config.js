678a3aea37ace05e5d3aae0143aeea983fef9d91
Anurag Singh <contact@anurag.sh>
2026-06-09 18:02:08 +0800

    remove branch prefix

--- platform-services/renovate/config.js
+++ platform-services/renovate/config.js
@@ -5,9 +5,6 @@ module.exports = {
   autodiscoverTopics: ["managed-by-renovate"],
   dependencyDashboard: true,
   gitAuthor: "renovate[bot] <noreply@git.anurag.sh>",
-  // Forgejo 404s on /commits/{ref}/statuses when ref contains an encoded slash,
-  // which Renovate maps to REPOSITORY_CHANGED and aborts the repo. Drop the slash.
-  branchPrefix: "renovate-",
   allowedUnsafeExecutions: ["gradleWrapper"],
   extends: ["config:recommended"],
 };
