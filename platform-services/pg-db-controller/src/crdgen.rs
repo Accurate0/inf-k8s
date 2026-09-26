2d99624c4b84cdae97c6ab2d35ef339653fc0eae
Anurag <contact@anurag.sh>
2026-09-26 19:16:00 +0800

    remove pg-db-controller

--- platform-services/pg-db-controller/src/crdgen.rs
+++
@@ -1,8 +0,0 @@
-use kube::CustomResourceExt;
-
-fn main() {
-    print!(
-        "{}",
-        serde_yaml::to_string(&pg_db_controller::PostgresDatabase::crd()).unwrap()
-    )
-}
--- platform-services/pg-db-controller/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod controller;
-pub mod crd;
-pub mod error;
-pub mod sql;
-
-pub use crd::{Condition, PgDatabaseSpec, PostgresDatabase, PostgresDatabaseStatus, IDENT_PATTERN};
-pub use error::{Error, Result};
